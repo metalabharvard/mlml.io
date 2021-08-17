ce9ffbbb98e82e74127fc403a1fa74f1ad010f62
z3to <6gqrbj@posteo.org>
2021-08-17 13:25:55 +0200

    fix not found items
    #19

--- assets/js/index.js
+++ assets/js/index.js
@@ -89,37 +89,39 @@ window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('[role="tab"]');
   const tabList = document.querySelector('[role="tablist"]');
 
-  // Add a click event handler to each tab
-  tabs.forEach(tab => {
-    tab.addEventListener('click', changeTabs);
-  });
+  if (tabs.length && tabList) {
+    // Add a click event handler to each tab
+    tabs.forEach(tab => {
+      tab.addEventListener('click', changeTabs);
+    });
 
-  // Enable arrow navigation between tabs in the tab list
-  let tabFocus = 0;
+    // Enable arrow navigation between tabs in the tab list
+    let tabFocus = 0;
 
-  tabList.addEventListener('keydown', e => {
-    // Move right
-    if (e.keyCode === 39 || e.keyCode === 37) {
-      tabs[tabFocus].setAttribute('tabindex', -1);
-      if (e.keyCode === 39) {
-        tabFocus++;
-        // If we're at the end, go to the start
-        if (tabFocus >= tabs.length) {
-          tabFocus = 0;
+    tabList.addEventListener('keydown', e => {
+      // Move right
+      if (e.keyCode === 39 || e.keyCode === 37) {
+        tabs[tabFocus].setAttribute('tabindex', -1);
+        if (e.keyCode === 39) {
+          tabFocus++;
+          // If we're at the end, go to the start
+          if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+          }
+          // Move left
+        } else if (e.keyCode === 37) {
+          tabFocus--;
+          // If we're at the start, move to the end
+          if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+          }
         }
-        // Move left
-      } else if (e.keyCode === 37) {
-        tabFocus--;
-        // If we're at the start, move to the end
-        if (tabFocus < 0) {
-          tabFocus = tabs.length - 1;
-        }
+
+        tabs[tabFocus].setAttribute('tabindex', 0);
+        tabs[tabFocus].focus();
       }
-
-      tabs[tabFocus].setAttribute('tabindex', 0);
-      tabs[tabFocus].focus();
-    }
-  });
+    });
+  }
 });
 
 function changeTabs(e) {
