63a0167e8757e7dcc4a74e25b0fa3277881aac0f
jnsprnw <6gqrbj@posteo.org>
2021-11-02 13:28:26 +0100

    script documentation

--- scripts/edit/script.js
+++ scripts/edit/script.js
@@ -1,6 +1,8 @@
+// This is a script to batch edit many projects, members or events.
+// This script goes through all projects, allows you to edit for example the description and pushes these edits to Strapi again.
 const axios = require('axios');
 
-const FOLDER = 'members';
+const FOLDER = 'members'; // Change this to projects, members or events.
 
 axios
   .get(`https://metalab-strapi.herokuapp.com/${FOLDER}/`)
@@ -8,10 +10,15 @@ axios
     res.data.forEach(item => {
       let { description, id } = item;
       if (description) {
+        // Fix the formatting of bold links
         // https://regex101.com/r/MdtcRW/1
         // description = description.replace(/\[\*\*(.{2,}?)\*\*\]\((.{2,}?)\)/gm, "**[$1]($2)**");
+
+        // Fix the formatting of em links
         // https://regex101.com/r/QUZTba/1
         // description = description.replace(/\[\*(.{2,}?)\*\]\((.{2,}?)\)/gm, "*[$1]($2)*");
+
+        // Change relative links to use shortcodes
         // https://regex101.com/r/isXGkS/1
         // https://regex101.com/r/isXGkS/2
         // https://regex101.com/r/isXGkS/3
@@ -20,7 +27,7 @@ axios
         // https://regex101.com/r/ToEPoI/1
         // https://regex101.com/r/ToEPoI/2
         description = description.replace(/\[(.{2,}?)\]\(https*:\/\/metalabharvard\.github\.io\/*\)/gm, '{{< link "" >}}$1{{< /link >}}')
-        // console.log(str)
+
         axios.put(`https://metalab-strapi.herokuapp.com/${FOLDER}/${id}`, { description })
       }
     })
